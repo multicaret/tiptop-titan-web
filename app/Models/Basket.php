bf3fe9fef389e65586bc9252b863850647883c42
Suhail MSK2 <msk286@gmail.com>
2021-03-14 15:05:59 +0300

    remove status from basket

--- app/Models/Basket.php
+++ app/Models/Basket.php
@@ -3,29 +3,28 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Basket extends Model
 {
-    const STATUS_IN_PROGRESS = 1;
-    const STATUS_COMPLETED = 2;
 
-
-    public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
 
-    public function chain(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    public function chain(): BelongsTo
     {
         return $this->belongsTo(Chain::class);
     }
 
-    public function branch(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    public function branch(): BelongsTo
     {
         return $this->belongsTo(Branch::class);
     }
 
-    public function products(): \Illuminate\Database\Eloquent\Relations\BelongsToMany
+    public function products(): BelongsToMany
     {
         return $this->belongsToMany(Product::class, 'basket_product', 'basket_id', 'product_id')
                     ->withTimestamps();
