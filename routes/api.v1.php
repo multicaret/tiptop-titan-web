cc2fae5777057973c303b27ed9b1eec571328c15
KawsaraMohamed MK <mkwsra@gmail.com>
2021-03-17 12:35:04 +0300

    rename `/basket` to `/baskets`

--- routes/api.v1.php
+++ routes/api.v1.php
@@ -51,8 +51,8 @@ Route::middleware('auth:api')
          Route::resource('orders', 'OrderController')->except(['edit', 'update']);
          Route::get('orders/checkout', 'OrderController@checkoutCreate');
          Route::post('orders/checkout', 'OrderController@checkoutStore');
-         Route::post('basket/add-remove-product', 'BasketController@addRemoveProduct');
-         Route::post('basket/clear-basket', 'BasketController@clearBasket');
+         Route::post('baskets/add-remove-product', 'BasketController@addRemoveProduct');
+         Route::post('baskets/clear-basket', 'BasketController@clearBasket');
 
 //         Route::get('users', 'UserController@index');
 //         Route::get('users/{id}', 'UserController@show');
