e5605e9983386f2bcd52472ebd206a8bd62dcece
Wael Khalifa <wnkhalifa99@gmail.com>
2021-03-18 18:09:59 +0300

    change unsignedFloat to double

--- database/migrations/2021_03_13_211500_create_baskets_table.php
+++ database/migrations/2021_03_13_211500_create_baskets_table.php
@@ -19,8 +19,8 @@ class CreateBasketsTable extends Migration
             $table->unsignedBigInteger('user_id')->index();
             $table->unsignedBigInteger('chain_id')->index();
             $table->unsignedBigInteger('branch_id')->index();
-            $table->unsignedFloat('total')->default(0);
-            $table->unsignedFloat('without_discount_total')->default(0);
+            $table->double('total')->default(0);
+            $table->double('without_discount_total')->default(0);
             $table->unsignedInteger('products_count')->default(0);
             $table->unsignedBigInteger('crm_id')->nullable();
             $table->unsignedBigInteger('crm_user_id')->nullable();
