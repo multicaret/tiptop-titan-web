b47613bf5c8b0b16bbf83effcd017138d344877d
KawsaraMohamed MK <mkwsra@gmail.com>
2021-03-18 18:05:28 +0300

    WIP google maps static image generator

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,6 +12,29 @@ class HomeController extends Controller
         return redirect()->route('admin.index');
     }
 
+    public function staticMap()
+    {
+        $apiKey = env('GOOGLE_MAPS_API');
+        $zoom = 15;
+        $dimensions = '414x212';
+
+        $marker1LocationLatitude = config('defaults.geolocation.latitude') + 0.001;
+        $marker1LocationLongitude = config('defaults.geolocation.longitude') + 0.001;
+        $marker1 = urlencode("color:blue|label:S|{$marker1LocationLatitude},{$marker1LocationLongitude}");
+        $marker2LocationLatitude = config('defaults.geolocation.latitude') + 0.002;
+        $marker2LocationLongitude = config('defaults.geolocation.longitude') + 0.002;
+        $marker2IconUrl = "http://titan.trytiptop.app/favicon.png?2";
+        $marker2 = urlencode("icon:{$marker2IconUrl}|{$marker2LocationLatitude},{$marker2LocationLongitude}");
+//        $marker2 = urlencode("color:green|label:G|{$marker2LocationLatitude},{$marker2LocationLongitude}");
+
+        $locationLatitude = ($marker1LocationLatitude + $marker2LocationLatitude) / 2;
+        $locationLongitude = ($marker1LocationLongitude + $marker2LocationLongitude) / 2;
+        $url = "https://maps.googleapis.com/maps/api/staticmap?center={$locationLatitude},{$locationLongitude}&zoom=$zoom&size=$dimensions&maptype=roadmap
+&markers={$marker1}&markers={$marker2}&key=$apiKey";
+
+        return '<img src="'.$url.'"/> <a href="'.$url.'">Link</a>';
+    }
+
     public function foo()
     {
         $super = User::first();
