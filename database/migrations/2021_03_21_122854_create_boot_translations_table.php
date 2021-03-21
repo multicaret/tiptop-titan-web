74b19dca7f3798ccff217b271ee38c3560c7288b
Ghaith <Ghaith.m.alhasan@gmail.com>
2021-03-21 13:40:42 +0300

    create boot migration/model

---
+++ database/migrations/2021_03_21_122854_create_boot_translations_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBootTranslationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('boot_translations', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('boot_id');
+            $table->string('title')->nullable();
+            $table->text('description')->nullable();
+            $table->json('instructions')->nullable();
+            $table->string('locale')->index();
+            $table->json('data_translated')->nullable();
+
+            $table->unique(['boot_id', 'locale']);
+
+            $table->foreign('boot_id')->references('id')->on('boots')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('boot_translations');
+    }
+}
