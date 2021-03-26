401a540d2edc4af5e9feba05f7152c9c662de504
KawsaraMohamed MK <mkwsra@gmail.com>
2021-03-26 05:52:28 +0300

    send back the address with order resource

--- app/Http/Resources/OrderResource.php
+++ app/Http/Resources/OrderResource.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Resources;
 
-use App\Models\Cart;
 use App\Models\Currency;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -20,11 +19,7 @@ class OrderResource extends JsonResource
     {
         return [
             'id' => (int) $this->id,
-//            TODO: uncommitted it when you work on addresses
-//            'address' => [
-//                'icon' => $this->address->icon,
-//                'alias' => $this->address->name,
-//            ],
+            'address' => new LocationResource($this->address),
             'completedAt' => [
                 'formatted' => $this->completed_at->format(config('defaults.date.short_format')),
                 'diffForHumans' => $this->completed_at->diffForHumans(),
