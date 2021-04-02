ce188032f8d57bc9df9094f5d1503dbeca3ffe17
KawsaraMohamed MK <mkwsra@gmail.com>
2021-04-02 22:31:31 +0300

    add KB shortcut to orders index view

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -13,4 +13,18 @@
 @push('scripts')
     @livewireScripts
     <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+    <script src="https://unpkg.com/hotkeys-js/dist/hotkeys.min.js"></script>
+    <script type="text/javascript">
+        hotkeys('ctrl+r,r', function (event, handler) {
+            switch (handler.key) {
+                case 'ctrl+r':
+                case 'r':
+                    window.location.reload();
+                    break;
+                default:
+                    alert(event);
+            }
+        });
+    </script>
+
 @endpush
