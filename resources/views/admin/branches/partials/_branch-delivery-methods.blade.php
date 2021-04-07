48f318c2b8edde286dd950aba6badc27a494f355
Wael Khalifa <wnkhalifa99@gmail.com>
2021-04-07 19:43:03 +0300

    fix branch delivery card

--- resources/views/admin/branches/partials/_branch-delivery-methods.blade.php
+++ resources/views/admin/branches/partials/_branch-delivery-methods.blade.php
@@ -32,14 +32,14 @@
                             @slot('attributes',['step'=>1,'min'=>1])
                         @endcomponent
                     </div>
-                    <div class="col-md-12 mt-3">
+                    <div class="col-md-7 mt-3">
                         @component('admin.components.form-group', ['name' => 'under_minimum_order_delivery_fee', 'type' => 'number'])
                             @slot('label', 'Under minimum order delivery fee')
                             @slot('value', is_null($branch->under_minimum_order_delivery_fee) ? 0 : $branch->under_minimum_order_delivery_fee)
                             @slot('attributes',['step'=>1,'min'=>1])
                         @endcomponent
                     </div>
-                    <div class="col-md-12 mt-3">
+                    <div class="col-md-5 mt-3">
                         @component('admin.components.form-group', ['name' => 'free_delivery_threshold', 'type' => 'number'])
                             @slot('label', 'Free delivery threshold')
                             @slot('value', is_null($branch->free_delivery_threshold) ? 0 : $branch->free_delivery_threshold)
@@ -85,14 +85,14 @@
                                 @slot('attributes',['step'=>1,'min'=>1])
                             @endcomponent
                         </div>
-                        <div class="col-md-12 mt-3">
+                        <div class="col-md-7 mt-3">
                             @component('admin.components.form-group', ['name' => 'restaurant_under_minimum_order_delivery_fee', 'type' => 'number'])
                                 @slot('label', 'Under minimum order delivery fee')
                                 @slot('value', is_null($branch->restaurant_under_minimum_order_delivery_fee) ? 0 : $branch->restaurant_under_minimum_order_delivery_fee)
                                 @slot('attributes',['step'=>1,'min'=>1])
                             @endcomponent
                         </div>
-                        <div class="col-md-12 mt-3">
+                        <div class="col-md-5 mt-3">
                             @component('admin.components.form-group', ['name' => 'free_delivery_threshold', 'type' => 'number'])
                                 @slot('label', 'Free delivery threshold')
                                 @slot('value', is_null($branch->restaurant_under_minimum_order_delivery_fee) ? 0 : $branch->restaurant_under_minimum_order_delivery_fee)
