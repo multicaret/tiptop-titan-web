8e054f5edd7c5ac460e213ce2ecc0fc334ca668e
Suhail MSK2 <msk286@gmail.com>
2021-04-08 11:57:21 +0300

    [Importer] add old categories model & add global scope to all old models

--- app/Models/OldModels/OldCategory.php
+++ app/Models/OldModels/OldCategory.php
@@ -9,20 +9,23 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 
-class OldProduct extends Model
+class OldCategory extends Model
 {
     use Translatable;
 
 
     protected $connection = 'mysql-old';
-    protected $table = 'jo3aan_dishes';
+    protected $table = 'cms_categories';
     protected $primaryKey = 'id';
     protected $with = ['translations'];
-    protected array $translatedAttributes = ['title', 'description', 'image'];
-    protected $translationForeignKey = 'dish_id';
+    protected array $translatedAttributes = ['title', 'description', 'icon', 'image'];
+    protected $translationForeignKey = 'category_id';
 
-    public const TYPE_DISCOUNT_PERCENTAGE = 'PERCENTAGE';
-    public const TYPE_DISCOUNT_CASH = 'CASH';
+    public const MEALS = 'MEALS';
+    public const KITCHENS = 'KITCHENS';
+    public const RESTAURANTS = 'RESTAURANTS';
+    public const DISHES = 'DISHES';
+    public const SIDE_DISHES = 'SIDE_DISHES';
 
 
 
@@ -34,16 +37,13 @@ class OldProduct extends Model
         });
     }
 
+
     public static function attributesComparing(): array
     {
         return [
             'id' => 'id',
-            'price' => 'price',
-            'discount' => 'price_discount_amount',
-            'discount_type' => 'price_discount_by_percentage',
-            'discount_deadline' => 'price_discount_finished_at',
-            'rating' => 'avg_rating',
-            'rating_count' => 'rating_count',
+            'parent_id' => 'parent_id',
+            'sort_order' => 'order_column',
             'created_at' => 'created_at',
             'updated_at' => 'updated_at',
         ];
