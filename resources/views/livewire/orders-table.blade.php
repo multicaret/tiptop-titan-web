53d9b6f0f6aa5b1281acef48d48bddc51def4d58
KawsaraMohamed MK <mkwsra@gmail.com>
2021-04-09 16:15:12 +0300

    minor change

--- resources/views/livewire/orders-table.blade.php
+++ resources/views/livewire/orders-table.blade.php
@@ -126,7 +126,7 @@
                                 @if($order->type == \App\Models\Order::CHANNEL_GROCERY_OBJECT)
                                     {{--<img src="/images/icons/food-delivery-186/svg/019-food tray.svg"
                                          alt="Food Orders" class="d-inline-block ui-w-20" title="Food">--}}
-                                    <i class="fas fa-shopping-basket fa-1x text-primary"></i>
+                                    <i class="fas fa-shopping-basket fa-1x text-success"></i>
                                 @else
                                     {{-- <img src="/images/icons/food-delivery-186/svg/021-food delivery.svg"
                                           alt="Grocery Orders" class="d-inline-block ui-w-20" title="Grocery">--}}
