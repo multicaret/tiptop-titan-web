51c6969c841804e0113603b470023d464d09821a
KawsaraMohamed MK <mkwsra@gmail.com>
2021-04-09 16:13:12 +0300

    change the icon of preparing status

--- resources/views/admin/orders/_partials/statuses/10.blade.php
+++ resources/views/admin/orders/_partials/statuses/10.blade.php
@@ -1,6 +1,6 @@
-{{--<lottie-player
+<lottie-player
     src="{{url('animations/pot.json')}}"
     background="transparent" speed="1" style="width: 40px; height: 40px;margin: 0 auto;" loop
     autoplay></lottie-player>
-&nbsp;--}}
-<i class="fas fa-stopwatch-20 fa-2x text-black d-block"></i>
+{{--&nbsp;--}}
+{{--<i class="fas fa-stopwatch-20 fa-2x text-black d-block"></i>--}}
