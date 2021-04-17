435615139288104a3ac5bcf1caf66da561196fe6
KawsaraMohamed MK <mkwsra@gmail.com>
2021-04-17 18:37:17 +0300

    add helper accessors to HasAppTypes

--- app/Traits/HasAppTypes.php
+++ app/Traits/HasAppTypes.php
@@ -15,6 +15,16 @@ trait HasAppTypes
     {
         return request()->type === self::getChannelsArray()[self::CHANNEL_GROCERY_OBJECT];
     }
+    
+    public static function getIsFoodAttribute(): bool
+    {
+        return request()->type === self::getChannelsArray()[self::CHANNEL_FOOD_OBJECT];
+    }
+
+    public static function getIsGroceryAttribute(): bool
+    {
+        return request()->type === self::getChannelsArray()[self::CHANNEL_GROCERY_OBJECT];
+    }
 
     /**
      * @param $query
