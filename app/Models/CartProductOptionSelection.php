e8132b600c87732dd727059a04ebd51aebdaafeb
suhail <msk286@gmail.com>
2021-04-18 11:19:11 +0300

    [WIP] add cart product option selection

---
+++ app/Models/CartProductOptionSelection.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Database\Eloquent\Relations\Pivot;
+
+class CartProductOptionSelection extends Pivot
+{
+    protected $casts = [
+        'product_option_object' => 'json',
+    ];
+
+    public function cartProduct(): BelongsTo
+    {
+        return $this->belongsTo(CartProduct::class, 'cart_product_id');
+    }
+
+    public function productOption(): BelongsTo
+    {
+        return $this->belongsTo(ProductOption::class, 'product_option_id');
+    }
+
+    public function selectable(): MorphTo
+    {
+        return $this->morphTo();
+    }
+}
