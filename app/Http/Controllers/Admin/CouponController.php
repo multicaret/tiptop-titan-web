cd59556e333d26bc2723246d68e0935a3227c6fb
Ghaith <Ghaith.m.alhasan@gmail.com>
2021-04-19 17:14:10 +0300

    [dashboard] fix storing of min_cart_value_allowed in coupons

--- app/Http/Controllers/Admin/CouponController.php
+++ app/Http/Controllers/Admin/CouponController.php
@@ -200,6 +200,7 @@ class CouponController extends Controller
         $coupon->discount_amount = json_decode($request->input('discount_amount'));
         $coupon->max_usable_count = $request->input('max_usable_count');
         $coupon->max_usable_count_by_user = $request->input('max_usable_count_by_user');
+        $coupon->min_cart_value_allowed = $request->input('min_cart_value_allowed');
         $coupon->has_free_delivery = $request->has('has_free_delivery') ? $request->input('has_free_delivery') : 0;
         $coupon->discount_by_percentage = $request->has('discount_by_percentage') ? json_decode($request->input('discount_by_percentage')) : 0;
         $coupon->max_allowed_discount_amount = $request->input('max_allowed_discount_amount');
