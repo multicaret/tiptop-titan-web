a704319364d1fcc8145badf7dbae88a8f5a7365a
Ghaith <Ghaith.m.alhasan@gmail.com>
2021-04-22 20:51:18 +0300

    [restaurant-api] create restaurant edit/update API

--- routes/api-restaurants.v1.php
+++ routes/api-restaurants.v1.php
@@ -6,16 +6,18 @@ Route::middleware('throttle:15')
      ->group(function () {
          /* auth related */
          Route::post('login', 'Auth\AuthController@login');
+//       Route::post('auth/{provider}', 'Auth\SocialiteController@handleProvider');
+
          /* boot related */
          Route::get('/boot', 'HomeController@boot');
          Route::get('/', 'HomeController@root');
 
+        /*restaurant related*/
+         Route::get('restaurants/{restaurant}/edit', 'BranchController@edit');
+         Route::put('restaurants/{restaurant}', 'BranchController@update');
+
          Route::get('categories/{groceryCategory}/products', 'CategoryController@products');
          Route::get('products/{id}', 'ProductController@show');
-
-         /* auth related */
-         Route::post('login', 'Auth\AuthController@login');
-//         Route::post('auth/{provider}', 'Auth\SocialiteController@handleProvider');
 
          /* generals */
          Route::get('privacy-policy', 'PostController@privacy');
@@ -30,7 +32,6 @@ Route::middleware('auth:sanctum')
 
          // Orders
          Route::get('orders/food', 'OrderController@indexFood');
-         Route::get('orders/create', 'OrderController@create');
          Route::post('orders', 'OrderController@store');
          Route::post('orders/{order}/delete', 'OrderController@destroy');
      });
