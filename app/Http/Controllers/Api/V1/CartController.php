605f7f52049c3c862b31e5e20a518552d3d89636
KawsaraMohamed MK <mkwsra@gmail.com>
2021-04-26 03:00:12 +0300

    minor fix in calculating cart total for food

--- app/Http/Controllers/Api/V1/CartController.php
+++ app/Http/Controllers/Api/V1/CartController.php
@@ -143,9 +143,7 @@ class CartController extends BaseApiController
                         $cartProduct->price += $optionPrice;
                     }
                 }
-                // Add Product price to calculated total product option prices
-                $cartProduct->price += $cartProduct->product->discounted_price;
-                // Add total product option prices * quantity
+                // Add total product option prices(aka: $cartProduct->price) * quantity
                 $cartProduct->total_price = $cartProduct->price * $cartProduct->quantity;
                 $this->updateCartPrices($cartProduct, $cart, 'increment', $cartProduct->quantity);
             }
