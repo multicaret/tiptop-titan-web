b5f90d04090c7004795774343c9a032c46ee359f
KawsaraMohamed MK <mkwsra@gmail.com>
2021-04-30 18:47:53 +0300

    add importer_id to products

--- database/migrations/2021_03_10_173870_create_products_table.php
+++ database/migrations/2021_03_10_173870_create_products_table.php
@@ -48,10 +48,13 @@ class CreateProductsTable extends Migration
             $table->timestamp('custom_banner_ended_at')->nullable();
             $table->boolean('is_storage_tracking_enabled')->nullable(true);
             $table->unsignedTinyInteger('on_mobile_grid_tile_weight')->default(3);
+            $table->unsignedBigInteger('importer_id')->nullable();
             $table->unsignedBigInteger('cloned_from_product_id')->nullable();
             $table->timestamps();
             $table->softDeletes();
 
+            $table->unique(['branch_id', 'importer_id']);
+            
             $table->foreign('creator_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('editor_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('chain_id')->references('id')->on('chains')->onDelete('cascade');
