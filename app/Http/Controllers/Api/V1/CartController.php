08faf932bd504e04633bcd4cc47dce23d3368112
roaa94 <roaa94khaddam@gmail.com>
2021-05-02 00:50:18 +0300

    fix in cart controller delete grocery product endpoint

--- app/Http/Controllers/Api/V1/CartController.php
+++ app/Http/Controllers/Api/V1/CartController.php
@@ -287,7 +287,7 @@ class CartController extends BaseApiController
     public function destroyGroceryProduct(Cart $cart, $productId): JsonResponse
     {
         \DB::beginTransaction();
-        $cartProduct = CartProduct::whereCartId($cart->id)->where('product_id', $productId);
+        $cartProduct = CartProduct::whereCartId($cart->id)->where('product_id', $productId)->first();
         $cart->total -= $cartProduct->product->discounted_price;
         $cart->without_discount_total -= $cartProduct->product->price;
         $cart->save();
