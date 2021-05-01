52e57f349d9ea172f22eedd150479b9d888851a6
KawsaraMohamed MK <mkwsra@gmail.com>
2021-05-01 03:56:58 +0300

    work on LP views

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -1,5 +1,5 @@
-@extends('layouts.frontend')
-@section('title',__('Home'))
-@section('content')
+{{--@extends('layouts.frontend')--}}
+{{--@section('title',__('Home'))--}}
+{{--@section('content')--}}
     @include('frontend.'. localization()->getCurrentLocale().'-home')
-@endsection
+{{--@endsection--}}
