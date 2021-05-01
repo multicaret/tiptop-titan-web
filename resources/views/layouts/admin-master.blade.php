91f533046c781155f8fe1f2c9d463928e8300e58
Wael Khalifa <wnkhalifa99@gmail.com>
2021-05-01 23:34:51 +0300

    fix notifications style in navbar

--- resources/views/layouts/admin-master.blade.php
+++ resources/views/layouts/admin-master.blade.php
@@ -19,7 +19,7 @@
 
     <!-- Icons. Uncomment required icon fonts -->
     <link rel="stylesheet" href="{{ asset('/admin-assets/fonts/fontawesome.css') }}">
-    <link rel="stylesheet" href="{{ asset('/admin-assets/css/custom.css') }}?ver=1.0">
+    <link rel="stylesheet" href="{{ asset('/admin-assets/css/custom.css') }}?ver=1.1">
     <link rel="stylesheet" href="{{ asset('/admin-assets/fonts/ionicons.css') }}">
     <link rel="stylesheet" href="{{ asset('/admin-assets/fonts/linearicons.css') }}">
     {{--    <link rel="stylesheet" href="{{ asset('/admin-assets/fonts/open-iconic.css') }}">--}}
