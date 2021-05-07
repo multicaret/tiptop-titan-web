ca2fd19cb3e2c6a771a121651982c31ceb3d5a67
roaa94 <roaa94khaddam@gmail.com>
2021-05-07 04:46:39 +0300

    add region english name to branch resource

--- app/Http/Resources/BranchResource.php
+++ app/Http/Resources/BranchResource.php
@@ -29,6 +29,7 @@ class BranchResource extends JsonResource
         return [
             'id' => $this->id,
             'title' => $this->title,
+            'regionEnglishName' => $this->region->english_name,
             'regionId' => $this->region_id,
             'cityId' => $this->city_id,
             'tiptopDelivery' => [
