102b5408fe7db4bf4d4e6ee6a3c02817601fc217
roaa94 <roaa94khaddam@gmail.com>
2021-05-07 04:49:43 +0300

    add region english name to food branch resource

--- app/Http/Resources/FoodBranchResource.php
+++ app/Http/Resources/FoodBranchResource.php
@@ -45,6 +45,7 @@ class FoodBranchResource extends JsonResource
         return [
             'id' => $this->id,
             'title' => $this->title,
+            'regionEnglishName' => $this->region->english_name,
             'regionId' => $this->region_id,
             'cityId' => $this->city_id,
             'tiptopDelivery' => [
