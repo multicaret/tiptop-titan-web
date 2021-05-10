a5d7246546714c90907d5541f273d0fced5e4603
Suhail <msk286@gmail.com>
2021-05-10 15:48:02 +0300

    add branch id to fillable on taxonomyModel
    Signed-off-by: Suhail <msk286@gmail.com>

--- app/Models/TaxonomyModel.php
+++ app/Models/TaxonomyModel.php
@@ -172,7 +172,19 @@ class TaxonomyModel extends Model implements HasMedia, ShouldHaveTypes, Translat
     protected $table = 'taxonomies';
     public $translationForeignKey = 'taxonomy_id';
     public $translationModel = TaxonomyTranslation::class;
-    protected $fillable = ['creator_id', 'editor_id', 'title', 'description', 'parent_id', 'type', 'order_column', 'left', 'right', 'uuid'];
+    protected $fillable = [
+        'creator_id',
+        'editor_id',
+        'title',
+        'description',
+        'parent_id',
+        'type',
+        'order_column',
+        'left',
+        'right',
+        'uuid',
+        'branch_id',
+    ];
     protected $translatedAttributes = ['title', 'description'];
     protected $with = [
         'translations'
