3a5d45a12eee30b994c5dfc5bb4b827759ff617c
KawsaraM MK <mkwsra@gmail.com>
2021-05-13 04:50:49 +0300

    increase pagination in products page

--- app/Http/Livewire/Products/ProductsTable.php
+++ app/Http/Livewire/Products/ProductsTable.php
@@ -39,7 +39,7 @@ class ProductsTable extends Component
             });
         }
         $products = $products->latest()
-                             ->paginate(10);
+                             ->paginate(30);
 
         return $products;
     }
