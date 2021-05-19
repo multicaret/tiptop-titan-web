b8410d66e4007904f72296999348aeeab645a520
nour <nour@trytiptop.app>
2021-05-19 23:29:08 +0300

    migration to add zoho books info id to branches table

---
+++ database/migrations/2021_05_19_231643_add_zoho_books_info_to_branches_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddZohoBooksInfoToBranchesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('branches', function (Blueprint $table) {
+            $table->string('zoho_books_id')->nullable();
+            $table->string('zoho_books_account_id')->nullable();
+            $table->string('zoho_delivery_item_id')->nullable();
+            $table->string('zoho_tiptop_delivery_item_id')->nullable();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('branches', function (Blueprint $table) {
+            //
+        });
+    }
+}
