6dcfb628f9a92546ec97ffaed6f75b0d02b578b0
KawsaraM MK <mkwsra@gmail.com>
2021-05-23 18:44:19 +0300

    add pagination to notifications index view

--- app/Http/Livewire/NotificationsIndex.php
+++ app/Http/Livewire/NotificationsIndex.php
@@ -3,13 +3,18 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class NotificationsIndex extends Component
 {
+    use WithPagination;
+
+    protected $paginationTheme = 'bootstrap';
+
     public function render()
     {
         $user = auth()->user();
-        $notifications = $user->notifications()->latest()->get();
+        $notifications = $user->notifications()->latest()->paginate(7);
 
         return view('livewire.notifications.index', compact('notifications'));
     }
