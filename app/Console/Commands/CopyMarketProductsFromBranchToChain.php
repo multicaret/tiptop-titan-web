d85dbeabbde9c9fcbe40fa3ab4221c2d66e6143c
KawsaraM MK <mkwsra@gmail.com>
2021-06-18 15:54:41 +0300

    contine importing market products

--- app/Console/Commands/CopyMarketProductsFromBranchToChain.php
+++ app/Console/Commands/CopyMarketProductsFromBranchToChain.php
@@ -41,30 +41,45 @@ class CopyMarketProductsFromBranchToChain extends Command
      */
     public function handle()
     {
-        $products = Product::with('media')->whereBranchId(DatumImporter::DEFAULT_BRANCH_ID)->active();
+        $products = Product::with('media')
+                           ->where('id', '>', 44776)
+                           ->whereBranchId(DatumImporter::DEFAULT_BRANCH_ID)
+                           ->active();
         $this->bar = $this->output->createProgressBar($products->count());
         $this->bar->start();
         foreach ($products->cursor() as $originalProduct) {
-            $newProduct = $originalProduct->replicateWithTranslations();
-            $newProduct->branch_id = null;
-            $newProduct->zoho_books_id = null;
-            $newProduct->view_count = 1;
-            $newProduct->search_count = 0;
-            $newProduct->avg_rating = 0;
-            $newProduct->rating_count = 0;
-            $newProduct->push();
-            $newProduct->categories()->sync($originalProduct->categories->pluck('id'));
+            \DB::beginTransaction();
+            try {
+                $newProduct = $originalProduct->replicateWithTranslations();
+                $newProduct->branch_id = null;
+                $newProduct->zoho_books_id = null;
+                $newProduct->view_count = 1;
+                $newProduct->search_count = 0;
+                $newProduct->avg_rating = 0;
+                $newProduct->rating_count = 0;
+                $newProduct->cloned_from_product_id = $originalProduct->id;
+                $newProduct->push();
+                $newProduct->categories()->sync($originalProduct->categories->pluck('id'));
 
-            $oldMediaItems = $originalProduct->getMedia('cover');
-            foreach ($oldMediaItems as $oldMedia) {
-                $oldMedia->copy($newProduct, 'cover', 's3');
+                $oldMediaItems = $originalProduct->getMedia('cover');
+                foreach ($oldMediaItems as $oldMedia) {
+                    $oldMedia->copy($newProduct, 'cover', 's3');
+                }
+
+                $oldMediaItems = $originalProduct->getMedia('gallery');
+                foreach ($oldMediaItems as $oldMedia) {
+                    $oldMedia->copy($newProduct, 'gallery', 's3');
+                }
+            } catch (\Exception $e) {
+                \DB::rollback();
+                $this->error("ProductId: $originalProduct->id , error: {$e->getMessage()}");
+                info('error while cloning market products', [
+                    'originalProduct' => $originalProduct,
+                    'message' => $e->getMessage(),
+                    'error' => $e,
+                ]);
             }
-
-            $oldMediaItems = $originalProduct->getMedia('gallery');
-            foreach ($oldMediaItems as $oldMedia) {
-                $oldMedia->copy($newProduct, 'gallery', 's3');
-            }
-
+            \DB::commit();
             $this->bar->advance();
 
         }
