82816af6c310484a55ce6db164af277cb2990c3b
nour <nour@trytiptop.app>
2021-06-21 14:46:52 +0300

    zoho analytics date update

--- app/Http/Controllers/Zoho/ZohoAnalyticsController.php
+++ app/Http/Controllers/Zoho/ZohoAnalyticsController.php
@@ -14,13 +14,14 @@ class ZohoAnalyticsController extends Controller
 
         $dateCondition = ' ';
         if ( ! empty($request->date)) {
-            $dateCondition = "AND o.created_at > '". $request->date ."'";
+            $dateCondition = "AND DATE(o.created_at) = DATE('". $request->date ."')";
         }
 
         $data = DB::select("
             SELECT  DATE(o.created_at) AS 'order creation date',
              o.reference_code AS 'order id' ,
              CASE WHEN o.status=20 THEN 'Delivered' ELSE 'Canceled' END AS 'status',
+             rt.name AS 'region',
              ct.name AS 'city',
              CASE WHEN o.type=1 THEN 'market' ELSE 'food' END AS 'order type',
              CASE WHEN o.is_delivery_by_tiptop=1 THEN 'tiptop' ELSE 'restaurant' END AS 'delivery type',
@@ -37,13 +38,15 @@ class ZohoAnalyticsController extends Controller
              GROUP_CONCAT(ogn.message SEPARATOR '  |  ') AS notes
             FROM orders o
             LEFT JOIN branches b ON o.branch_id = b.id
-            LEFT JOIN branch_translations bt ON bt.branch_id = b.id AND bt.locale='ar'
+            LEFT JOIN branch_translations bt ON bt.branch_id = b.id AND bt.locale='en'
             LEFT JOIN users u ON u.id = o.driver_id
-            LEFT JOIN city_translations ct ON ct.city_id = o.city_id AND ct.locale = 'ar'
+            LEFT JOIN cities c ON c.id = o.city_id
+            LEFT JOIN city_translations ct ON ct.city_id = c.id AND ct.locale = 'en'
+            LEFT JOIN region_translations rt ON rt.region_id = c.region_id AND rt.locale = 'en'
             LEFT JOIN order_agent_notes ogn ON ogn.order_id = o.id
             LEFT JOIN locations l ON l.id = o.address_id
             WHERE (o.customer_notes NOT LIKE '%test%' OR o.customer_notes IS NULL) ".$dateCondition."
-            GROUP BY DATE(o.created_at), o.reference_code,bt.title,o.status,o.coupon_discount_amount,total_before,final_price,o.delivery_fee,driver,ct.name,o.type,'delivery type',b.latitude,b.longitude
+            GROUP BY DATE(o.created_at), o.reference_code,bt.title,o.status,o.coupon_discount_amount,total_before,final_price,o.delivery_fee,driver,rt.name,ct.name,o.type,'delivery type',b.latitude,b.longitude
              ,o.user_id,l.latitude,l.longitude");
         $filename = "restaurant-sales.json";
         $handle = fopen($filename, 'w+');
