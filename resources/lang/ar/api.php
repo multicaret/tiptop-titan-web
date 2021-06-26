043b101ac878fbe1363213e3f384b141f4cb4bf1
KawsaraM MK <mkwsra@gmail.com>
2021-06-26 14:47:19 +0300

    minor fix in translations

--- resources/lang/ar/api.php
+++ resources/lang/ar/api.php
@@ -4,5 +4,5 @@ return [
     'address_kind_Other' => 'آخر',
     'address_kind_Work' => 'عمل',
     'address_kind_Home' => 'منزل',
-    'No Branch is available in your area now! please check again at :time' => ':time لا يوجد فرع يقوم بالخدمة الان، يرجى معاودة المحاولة عند',
+    'No Branch is available in your area now! please check again at :time' => 'لا يوجد فرع يقوم بالخدمة الان، يرجى معاودة المحاولة عند :time',
 ];
