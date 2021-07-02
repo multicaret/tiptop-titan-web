9cd39eb21ce8a370216864e4da2de664427fd7f9
KawsaraM MK <mkwsra@gmail.com>
2021-07-02 13:50:39 +0300

    add horizon:snapshot command every 5min

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -32,8 +32,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')->hourly();
 //        $schedule->command(UpdateCurrencyExchangeRate::class)->hourly();
 
-//        $schedule->command('backup:clean')->daily()->at('01:00');
-//        $schedule->command('backup:run')->daily()->at('02:00');
+        $schedule->command('horizon:snapshot')->everyFiveMinutes();
         $schedule->command('branch-availability:update')->daily()->at('03:56');
 
         if (app()->environment('production')) {
