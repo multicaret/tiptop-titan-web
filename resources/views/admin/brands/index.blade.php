cc61a9c33d0d03a9c62edbf09f8429f76bdc1116
nour <nour@trytiptop.app>
2021-07-07 18:16:56 +0300

    fixed brands index page title again

--- resources/views/admin/brands/index.blade.php
+++ resources/views/admin/brands/index.blade.php
@@ -2,7 +2,7 @@
 @section('title', 'Brands')
 @section('content')
     <h4 class="d-flex justify-content-between align-items-center w-100 font-weight-bold py-3 mb-4">
-        {{trans('strings.captain_teams')}}
+        Brands
         <a href="{{ route('admin.brands.create') }}">
             <button type="button" class="btn btn-primary rounded-pill d-block">
                 <span class="ion ion-md-add"></span>
