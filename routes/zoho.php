bf704663c7fac182fd53fe15cbc32dc55fbe654f
KawsaraM MK <mkwsra@gmail.com>
2021-07-27 13:41:58 +0300

    add zoho webhooks

--- routes/zoho.php
+++ routes/zoho.php
@@ -4,3 +4,9 @@ Route::get('analytics/authorize', 'ZohoAnalyticsController@authorizeRequest');
 Route::get('analytics/restaurant-daily-orders/import', 'ZohoAnalyticsController@dailyOrdersJsonImport');
 Route::get('analytics/branches/import', 'ZohoAnalyticsController@branchesJsonImport');
 Route::get('analytics/search-terms/import', 'ZohoAnalyticsController@searchTermsJsonImport');
+Route::get('inventory/adjust-quantity-for-new-products', function () {
+    return 'OK';
+});
+Route::get('inventory/transfer-quantity-between-branches', function () {
+    return 'OK';
+});
