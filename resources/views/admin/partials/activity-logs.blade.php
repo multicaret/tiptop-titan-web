abdee07434e3500a98b77005b4f4dfde628ba955
KawsaraM MK <mkwsra@gmail.com>
2021-07-29 10:59:45 +0300

    !retrieve avg rating type of activities

--- resources/views/admin/partials/activity-logs.blade.php
+++ resources/views/admin/partials/activity-logs.blade.php
@@ -1,18 +1,20 @@
 <ul class="vertical-timeline" style="height:360px;overflow-y: scroll;padding-top:15px;margin-bottom: 2.5rem;">
     @php
-        $activities = $object->activity()->latest()->get();
+        $activities = $object->activity()
+                             ->with('user.media')
+                             ->latest()
+                             ->where('differences','not like','%avg_rating%')
+                             ->get();
         $classNameLowerCase = strtolower(class_basename($object));
     @endphp
     @if($activities->count())
         @foreach($activities as $activity)
             <li class="rings border-{{$activity->getCssClassesBasedOnType()}}">
-                <span></span>
                 <div class="timeline-content">
                     <div class="title text-{{$activity->getCssClassesBasedOnType()}}"><i
                             class="{{$activity->getFontAwesomeClassesBasedOnType()}}"></i>
                         {{trans('activity.'.strstr($activity->type,"_$classNameLowerCase",1))}}
                     </div>
-{{--                    {{dd($activity)}}--}}
                     @if($activity->differences)
                         <div class="{{$activity->getCssClassesBasedOnType()}}">
                             <ul>
@@ -27,11 +29,6 @@
                             </ul>
                         </div>
                     @endif
-                    <div class="{{$activity->getCssClassesBasedOnType()}}">
-                        {{--                        <span>Record Update date: </span>--}}
-                        {{--                        &nbsp--}}
-                        {{--                        <span>{{$activity->z_at->format(config('defaults.datetime.normal_format'))}}</span>--}}
-                    </div>
                     <div class="edit-by">
                         <div class="person">
                             <img src="{{$activity->user->avatar}}" alt="{{$activity->user->name}}"/>
@@ -40,9 +37,9 @@
                     </div>
                 </div>
                 <span class="date-and-time">
-                <span class="time">{{$activity->created_at->format('H:i')}}</span>
-                <span class="date">{{$activity->created_at->format('Y-m-d')}}</span>
-            </span>
+                    <span class="time">{{$activity->created_at->format('H:i')}}</span>
+                    <span class="date">{{$activity->created_at->format('Y-m-d')}}</span>
+                </span>
             </li>
         @endforeach
     @endif
