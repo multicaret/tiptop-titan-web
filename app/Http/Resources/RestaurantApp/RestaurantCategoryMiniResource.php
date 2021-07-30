a2d109f46edb6fcfb39fd9487986cb21254acb7e
KawsaraM MK <mkwsra@gmail.com>
2021-07-30 15:39:19 +0300

    minor fixes for restaurant API

--- app/Http/Resources/RestaurantApp/RestaurantCategoryMiniResource.php
+++ app/Http/Resources/RestaurantApp/RestaurantCategoryMiniResource.php
@@ -18,23 +18,23 @@ class RestaurantCategoryMiniResource extends JsonResource
      */
     public function toArray($request)
     {
-        $products = null;
         $isMenuCategory = $this->type == Taxonomy::TYPE_MENU_CATEGORY;
-        if ($isMenuCategory) {
-            $menuProducts = $this->menuProducts();
-            $user = auth('sanctum')->user();
-            if (is_null($user) || ( ! is_null($user) && ! $user->is_branch_manager)) {
-                $menuProducts = $menuProducts->notDraft();
-            }
-            $products = RestaurantProductMiniResource::collection($menuProducts->orderByDesc('order_column')->get());
+//        if ($isMenuCategory) {
+        $menuProducts = $this->menuProducts();
+        $user = auth('sanctum')->user();
+        if (is_null($user) || ( ! is_null($user) && ! $user->is_branch_manager)) {
+            $menuProducts = $menuProducts->notDraft();
         }
+        $products = RestaurantProductMiniResource::collection($menuProducts->orderByDesc('order_column')->get());
+
+//        }
 
         return [
             'id' => $this->id,
             'icon' => $this->icon,
             'title' => $this->title,
             'hasChildren' => $this->hasChildren(),
-            'products' => $this->when($isMenuCategory, $products),
+            'products' => $products,
         ];
     }
 }
