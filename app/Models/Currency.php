7a16ec1ccf945d495d9cd762ab4be7cf40a05ec0
KawsaraM MK <mkwsra@gmail.com>
2021-08-01 16:17:58 +0300

    enhance currency

--- app/Models/Currency.php
+++ app/Models/Currency.php
@@ -86,6 +86,16 @@ class Currency extends Model
         static::addGlobalScope(new ActiveScope);
     }
 
+    public static function retrieve($currencyCode)
+    {
+        return cache()->remember('currency_'.$currencyCode, 60 * 60 * 1,
+            function () use ($currencyCode) {
+                return self::withoutGlobalScope(ActiveScope::class)
+                           ->where('code', $currencyCode)
+                           ->first();
+            });
+    }
+
     /**
      * @param $currencyCode
      *
@@ -93,14 +103,14 @@ class Currency extends Model
      */
     private static function getCurrency($currencyCode)
     {
-        $builder = self::withoutGlobalScope(ActiveScope::class);
         if (is_null($currencyCode)) {
-            $currency = $builder->where('code', config('app.default_currency_code'))->first();
             if (session()->has('currency_code')) {
-                $currency = $builder->where('code', session()->get('currency_code'))->first();
+                $currency = self::retrieve(session()->get('currency_code'));
+            } else {
+                $currency = self::retrieve(config('app.default_currency_code'));
             }
         } else {
-            $currency = $builder->where('code', $currencyCode)->first();
+            $currency = self::retrieve($currencyCode);
         }
 
         return $currency;
